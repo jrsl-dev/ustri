78829f47b4862c1b3371927b4c3e07cbcd1ff0d1
James Russell <jrrussell1224@gmail.com>
2018-01-14 15:31:14 -0500

    more initial set up

---
+++ src/components/USTRI.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+
+const Title = styled.span`
+  display: block;
+  width: 16ch;
+  padding-bottom: 0.15em;
+  border-bottom: 2px solid;
+`;
+
+const SubTitle = styled.span`
+  display: block;
+  font-size: 75%;
+`;
+export default () => (
+  <div>
+    <Title>United States Tamiya Ryu Iaijutsu</Title>
+    <SubTitle>Gengo Hojisha no Kai</SubTitle>
+  </div>
+);
