dbf553367011a5264e5175b01ead3490523a73db
James Russell <jrrussell1224@gmail.com>
2018-01-20 13:58:10 -0500

    sets minmax width on grid, adds bottom margin to heading

--- src/components/Sections.js
+++ src/components/Sections.js
@@ -3,12 +3,21 @@ import styled from "styled-components";
 
 const Base = styled.section`
   display: grid;
-  grid-template-columns: minmax(25ch, 65ch);
+  grid-template-columns: minmax(25ch, 60ch);
   justify-content: center;
   padding: 3.5rem;
 
   & > a:last-child {
     justify-self: center;
+  }
+
+  & > h1:first-child,
+  & > h2:first-child,
+  & > h3:first-child,
+  & > h4:first-child,
+  & > h5:first-child,
+  & > h6:first-child {
+    margin-bottom: 2em;
   }
 `;
 
@@ -32,7 +41,11 @@ export default { White, Light, Dark };
 export const Subsection = styled.div`
   display: grid;
   max-width: 30rem;
-  margin: 0 auto 6rem;
+  margin: 0 auto 4rem;
+
+  &:last-child {
+    margin-bottom: 0;
+  }
 
   & > img,
   & > a:last-child {
