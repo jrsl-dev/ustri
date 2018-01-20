feeeac4eec625e9d9925f7bc3de6795f88351673
James Russell <jrrussell1224@gmail.com>
2018-01-20 14:00:01 -0500

    adds styling for kikentai section

--- src/paritals/Home/KiKenTai.js
+++ src/paritals/Home/KiKenTai.js
@@ -1,42 +1,72 @@
 import React from "react";
+import styled from "styled-components";
 import Sections from "../../components/Sections";
+import ki from "../../assets/ki-kanji.svg";
+import ken from "../../assets/ken-kanji.svg";
+import tai from "../../assets/tai-kanji.svg";
+
+const Media = styled.div`
+  display: grid;
+  grid-template-columns: 22ch;
+  grid-template-rows: auto auto 1fr;
+  justify-content: center;
+
+  > img {
+    justify-self: center;
+  }
+
+  > h3 {
+    justify-self: center;
+  }
+`;
 
 const Ki = () => (
-  <div>
-    <img src="images/ki-kanji.svg" />
+  <Media>
+    <img src={ki} alt="ki" />
     <h3>Ki</h3>
     <p>
       Spirit. Tamiya Ryu trains the spirit and aims to create a harmonious
       character.
     </p>
-  </div>
+  </Media>
 );
 
 const Ken = () => (
-  <div>
-    <img src="images/ken-kanji.svg" />
+  <Media>
+    <img src={ken} alt="ken" />
     <h3>Ken</h3>
     <p>
       Sword. The sword of Tamiya Ryu is one that gives life or katsujin no ken.
     </p>
-  </div>
+  </Media>
 );
 
 const Tai = () => (
-  <div>
-    <img src="images/tai-kanji.svg" />
+  <Media>
+    <img src={tai} alt="tai" />
     <h3>Tai</h3>
     <p>
       Body. By harmonizing the spirit, sword, and body, the practice of Iaijutsu
       becomes dignified.
     </p>
-  </div>
+  </Media>
 );
 
+const KiKenTai = Sections.White.extend`
+  grid-column-gap: 2em;
+  grid-row-gap: 4em;
+  margin-top: 6rem;
+  margin-bottom: 6rem;
+
+  @media (min-width: 52rem) {
+    grid-template-columns: 22ch 22ch 22ch;
+  }
+`;
+
 export default () => (
-  <Sections.White>
+  <KiKenTai>
     <Ki />
     <Ken />
     <Tai />
-  </Sections.White>
+  </KiKenTai>
 );
