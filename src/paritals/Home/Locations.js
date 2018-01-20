25f1005bf51bc1a7492d1e0ce8a6c4c54ee23a9b
James Russell <jrrussell1224@gmail.com>
2018-01-19 23:05:32 -0500

    adds location subsections

--- src/paritals/Home/Locations.js
+++ src/paritals/Home/Locations.js
@@ -1,11 +1,13 @@
 import React from "react";
+import styled from "styled-components";
+import Link from "../../components/CTA";
 import Card from "../../components/Card";
-import Sections from "../../components/Sections";
+import Sections, { Subsection } from "../../components/Sections";
 import honbu from "../../assets/honbu-dojo.jpg";
 import macomb from "../../assets/macomb-dojo.jpg";
 
 const EastLansing = () => (
-  <div>
+  <Subsection>
     <h3>East Lansing, Michigan</h3>
     <img src={honbu} alt="outside view of the michigan honbu dojo" />
     <h4>Michigan Honbu Dojo</h4>
@@ -13,12 +15,12 @@ const EastLansing = () => (
       The Michigan Honbu dojo is the headquarters for Tamiya Ryu Iajiutsu
       operations in the United States.
     </p>
-    <a href="/east-lansing-michigan-honbu-dojo.html">Learn more</a>
-  </div>
+    <Link to="/locations/east-lansing-michigan">Learn more</Link>
+  </Subsection>
 );
 
 const Macomb = () => (
-  <div>
+  <Subsection>
     <h3>Macomb, Michigan</h3>
     <img src={macomb} alt="inside view of the southeast michigan branch dojo" />
     <h4>Southeast Michigan Branch Dojo</h4>
@@ -26,52 +28,32 @@ const Macomb = () => (
       The Southeast Michigan Branch dojo holds class at Macomb Township Parks
       &amp; Recreation Center.
     </p>
-    <a href="/southeast-michigan-branch-dojo.html">Learn more</a>
-  </div>
+    <Link to="/locations/macomb-michigan">Learn more</Link>
+  </Subsection>
 );
 
 const Brighton = () => (
-  <div>
+  <Subsection>
     <h3>Brighton, Michigan</h3>
-    <img src="images/macomb-location.jpg" />
+    {/* <img src="images/macomb-location.jpg" /> */}
     <h4>Brighton Study Group</h4>
     <p>
       The Brighton Study Group operates under the Southeast Michigan Branch Dojo
       and holds class at the Brighton Education and Community Center.
     </p>
-    <a href="/brighton-michigan/">Learn more</a>
-  </div>
+    <Link to="/brighton-michigan/">Learn more</Link>
+  </Subsection>
 );
 
-const LocationAnnouncement = () => (
-  <Card icon-megaphone>
-    <h3>Announcement</h3>
-    <h4>Southeast Michigan Branch To Move Operations!</h4>
-    <p>
-      We would like to announce that the Southeast Michigan Branch Dojo will be
-      moving its operations to Macomb Township starting in 2017. Classes will
-      still be held on <strong>Saturdays from 10:30am to 12:00pm</strong> at the
-      Macomb Township Community Recreation Center.
-    </p>
-    <p>
-      Registration will be handled through Macomb Township Parks &amp;
-      Recreation.
-    </p>
-    <p>
-      Please visit <a href="http://www.gomtpr.org/">gomtpr.org</a> for
-      registration details. Other inquiries should be directed to Genzan sensei
-      at <a href="mailto:genzan@ustamiyaryu.org">genzan@ustamiyaryu.org</a>.
-    </p>
-    <p>We hope to see you there!</p>
-  </Card>
-);
+const Heading = styled.h2`
+  margin-bottom: 3rem;
+`;
 
 export default () => (
   <Sections.White>
-    <h2>Locations</h2>
+    <Heading>Locations</Heading>
     <EastLansing />
     <Macomb />
-    <LocationAnnouncement />
     <Brighton />
   </Sections.White>
 );
