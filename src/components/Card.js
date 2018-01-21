1533e7e82c8a256516d9f57d6ac6e9d439bf7080
James Russell <jrrussell1224@gmail.com>
2018-01-21 06:19:39 -0500

    centers last anchor for cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,6 +7,12 @@ const Card = styled.div`
   border-radius: 3px;
   box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.5);
   background: #f5f5f5;
+  display: flex;
+  flex-direction: column;
+
+  & > a:last-child {
+    align-self: center;
+  }
 `;
 
 export default ({ children }) => <Card>{children}</Card>;
