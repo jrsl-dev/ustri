ea32d4485ceb92b4dedb947c1f454b8b14650a88
James Russell <jrrussell1224@gmail.com>
2018-01-21 19:56:53 -0500

    inazuma ken

--- src/paritals/Macomb/index.js
+++ src/paritals/Macomb/index.js
@@ -1,14 +1,22 @@
 import React from "react";
+import styled from "styled-components";
 import Sections from "../../components/Sections";
 import Introduction from "./Introduction";
 import Schedule from "./Schedule";
 import Contact from "./Contact";
+import inazuma from "../../assets/inazuma-ken-kanji.svg";
+
+const Kanji = styled.img`
+  max-width: 15rem;
+  justify-self: center;
+  margin: 4rem 0;
+`;
 
 export default ({ Section = Sections.White }) => (
   <Section>
     <Introduction />
     <Schedule />
-    <img src="images/inazuma-ken-kanji.svg" />
+    <Kanji src={inazuma} />
     <Contact />
   </Section>
 );
