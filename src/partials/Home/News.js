e5985074f976cf61860c0423e4913774ceb358c2
James Russell <jrrussell1224@gmail.com>
2020-04-18 11:12:23 -0400

    adds pandemic response announcement

--- src/partials/Home/News.js
+++ src/partials/Home/News.js
@@ -35,6 +35,64 @@ export default () => (
     <NewsFeed>
       <li>
         <Card>
+          <h3>Announcement</h3>
+          <h4>USTRI Gennankai Response to the Current Pandemic</h4>
+          <p>
+            Due to these uncertain and challenging times we currently find
+            ourselves in, I would like to take this opportunity to update all of
+            you as to our response to the Corona Virus situation and what lies
+            ahead for our organization.
+          </p>
+
+          <p>
+            For the health and safety of all members of the Gennankai, all of
+            our Dojo (Honbu, Southeast Michigan and Brighton) are currently
+            closed until July 1. While our sincere intention is to resume
+            regular Training and Classes in July, it will depend on whether or
+            not the State of Emergency and the Stay at Home Order have been
+            lifted by the Governor. If we are in the clear as of July 1, all our
+            Dojo will re-open and we will be accepting new students once again.
+            New Student Interviews will also resume for those who have completed
+            their Introductory Training. Barring any unforeseen issues, Training
+            will continue as normal for the balance of 2020. Also, our May
+            All-Member Practice and July Shinsakai and Koushuukai (Testing and
+            Lecture) have been cancelled for this year.
+          </p>
+
+          <p>
+            Unfortunately, the visit to Michigan this coming July 16-21 by our
+            15th Soke and his Assistant, Endo Tsuyako ( Gentei) Sensei, has been
+            put on hold until July 15-20 of 2021. Due to the fact that both of
+            them are in a "high-risk" group because of their age and certain
+            health issues, upon close consultation with both of them, we decided
+            that this is the best course of action at this time. Despite the
+            worsening conditions in Japan at this time, Soke Sensei, Endo Sensei
+            and all the members of the Tamiya Ryu Iaijutsu Genwakai are in good
+            health and doing their best to follow the guidelines set forth by
+            the Japanese Government in order to "flatten the curve." The
+            thoughts and prayers of all members of the Gennankai are with our
+            counterparts in Japan and Canada in the hope that all of them come
+            through this Pandemic unaffected!
+          </p>
+
+          <p>
+            In closing, my earnest thoughts and prayers for good health and
+            safety are with all of YOU during this unsettled time. Please "stay
+            home, stay safe and save lives" and always remember, "we are all in
+            this together!"
+          </p>
+
+          <p>Respectfully submitted,</p>
+
+          <p>
+            Michael Alexanian ( Gennan Buhaku )<br />
+            <br />
+            Kaicho (President), United States Tamiya Ryu Iaijutsu Gennankai
+          </p>
+        </Card>
+      </li>
+      <li>
+        <Card>
           <h3>Message from the Head Instructor</h3>
           <p>Gennan Buhaku sensei shares his thoughts on various topics</p>
           <a href="/resources/">Read More</a>
@@ -65,16 +123,6 @@ export default () => (
             Wung (Genkou) were officailly licensed as instructors under the
             United States Tamiya Ryu Iaijutsu Gennankai.
           </p>
-        </Card>
-      </li>
-      <li>
-        <Card>
-          <h3>Announcement</h3>
-          <p>
-            We are excited to announce the Michigan Honbu in East Lansing is
-            offering a <b>free introductory month</b>.
-          </p>
-          <a href="/locations/east-lansing-michigan">Learn More</a>
         </Card>
       </li>
       <li>
