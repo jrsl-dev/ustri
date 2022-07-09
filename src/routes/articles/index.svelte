5f74e817a298194fc715ec17ee075eac81d1da76
jrl <jrrussell1224@gmail.com>
2022-07-09 17:40:53 -0400

    get both message and reflection content and archive

--- src/routes/articles/index.svelte
+++ src/routes/articles/index.svelte
@@ -1,16 +1,54 @@
 <script context="module">
-  // get both current articles
-  // get both archives
+  export async function load({ fetch }) {
+    // get both current articles
+    const currentMessage = await fetch("/api/articles/messages/current.json");
+    const { content: messageContent } = await currentMessage.json();
+    const messageArchive = await fetch("/api/articles/messages/archive.json");
+    const messages = await messageArchive.json();
+
+    // get both archives
+    const currentReflection = await fetch(
+      "/api/articles/reflections/current.json"
+    );
+    const { content: reflectionContent } = await currentReflection.json();
+    const reflectionArchive = await fetch(
+      "/api/articles/reflections/archive.json"
+    );
+    const reflections = await reflectionArchive.json();
+
+    return {
+      props: {
+        messageContent,
+        reflectionContent,
+        messages,
+        reflections,
+      },
+    };
+  }
 </script>
 
 <script>
   // @ts-nocheck
+  export let messageContent;
+  export let reflectionContent;
+  export let messages;
+  export let reflections;
 </script>
 
 <section>
-  <article>Current Message</article>
-
-  <article>Current Reflection</article>
+  <article>{@html messageContent.html}</article>
+  <article>{@html reflectionContent.html}</article>
 </section>
 
-<aside>Archieved articles</aside>
+<aside>
+  <ol>
+    {#each messages as article}
+      <li><a href={article.url}>{article.title}</a></li>
+    {/each}
+  </ol>
+  <ol>
+    {#each reflections as article}
+      <li><a href={article.url}>{article.title}</a></li>
+    {/each}
+  </ol>
+</aside>
