ab42ad60fb3cc0ae99e9ae9231f0b6cb81b63189
jrl <jrrussell1224@gmail.com>
2022-07-13 19:16:59 -0400

    articles layout updates

---
+++ src/lib/components/ByLine.svelte
@@ -0,0 +1,22 @@
+<script>
+  export let title, author, date;
+</script>
+
+<header>
+  <h3>{title}</h3>
+  <i>{author}, {date}</i>
+</header>
+
+<style>
+  h3 {
+    border-bottom: 3px solid;
+    padding-bottom: 4px;
+    font-size: 1.35em;
+  }
+
+  i {
+    display: block;
+    margin: -1.3rem 0 0 0;
+    text-align: right;
+  }
+</style>
--- src/routes/articles/_articles.svelte
+++ src/routes/articles/_articles.svelte
@@ -1,38 +1,16 @@
 <script>
   import Highlight from "$lib/components/Highlight.svelte";
+  import ByLine from "$lib/components/ByLine.svelte";
 
-  export let title;
-  export let date;
-  export let author;
+  export let title = "";
+  export let date = "";
+  export let author = "";
   export let category = "Article";
 </script>
 
 <h2 class="main-heading">{category}</h2>
 
 <Highlight>
-  <header>
-    <h3>{title}</h3>
-    <div>
-      <p>{author}</p>
-      <p>Published {date}</p>
-    </div>
-  </header>
-
+  <ByLine {title} {author} {date} />
   <slot />
 </Highlight>
-
-<style>
-  header {
-    margin-bottom: 2rem;
-  }
-
-  div {
-    display: flex;
-    justify-content: space-between;
-    margin-top: -1rem;
-  }
-
-  p {
-    margin: 0;
-  }
-</style>
