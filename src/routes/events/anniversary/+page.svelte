154481046c58f56d0fc0daca9f1866ed037b540c
James <jrrussell1224@gmail.com>
2026-08-06 13:00:36 -0400

    adds some meta data, updates seating

--- src/routes/events/anniversary/+page.svelte
+++ src/routes/events/anniversary/+page.svelte
@@ -4,6 +4,33 @@
   import anniversary from "$lib/assets/30th-anniversary.jpg";
 </script>
 
+<svelte:head>
+  <title>
+    United States Tamiya Ryu Iaijutsu Gennankai | 30th Anniversary Enbukai
+  </title>
+  <meta
+    name="description"
+    content="To celebrate the 30 year anniversary of the founding of American Tamiya Ryu Iaijutsu, an Enbukai (Demonstration Event) will be held on September 20, 2026, at the East Lansing Hannah Community Center East Gym from 1-3pm. This event is free and open to the general public and will feature Enbu (formal demonstrations) by 15th Generation Soke Tsumaki Kazuo Genwa, the Senior Teaching Staff, the Head Instructor and the members of the Gennankai."
+  />
+  <meta
+    property="og:title"
+    content="United States Tamiya Ryu Iaijutsu Gennankai | 30th Anniversary Enbukai"
+  />
+  <meta
+    property="og:description"
+    content="United States Tamiya Ryu Iaijutsu Gennankai will hold a special 30th anniversary Enbukai (Demonstration Event) on September 20, 2026 in East Lansing, Michigan."
+  />
+  <meta property="og:image" content="/images/30th-anniversary.jpg" />
+  <meta
+    property="og:url"
+    content="https://www.ustamiyaryu.org/events/anniversary"
+  />
+  <meta property="og:type" content="website" />
+  <meta
+    property="og:site_name"
+    content="United States Tamiya Ryu Iaijutsu Gennankai"
+  />
+</svelte:head>
 <section>
   <Highlight>
     <h2>30th Anniversary Enbukai</h2>
@@ -18,13 +45,13 @@
       To celebrate the 30 year anniversary of the founding of American Tamiya
       Ryu Iaijutsu, an Enbukai (Demonstration Event) will be held on September
       20, 2026, at the East Lansing Hannah Community Center East Gym from 1-3pm.
-      This event is free and open to the general public and will feature Enbu
-      (formal demonstrations) by 15th Generation Soke Tsumaki Kazuo Genwa, the
-      Senior Teaching Staff, the Head Instructor and the members of the
-      Gennankai. We will be inviting the current Consul General of Japan, the
-      Mayors of Lansing and East Lansing and the Visiting Official from
-      Michigan’s Sister State of Shiga Prefecture to attend and say a few words
-      to commemorate the occasion just as they did 30 years ago.
+      This event is free and open to the general public (seating limited) and
+      will feature Enbu (formal demonstrations) by 15th Generation Soke Tsumaki
+      Kazuo Genwa, the Senior Teaching Staff, the Head Instructor and the
+      members of the Gennankai. We will be inviting the current Consul General
+      of Japan, the Mayors of Lansing and East Lansing and the Visiting Official
+      from Michigan’s Sister State of Shiga Prefecture to attend and say a few
+      words to commemorate the occasion just as they did 30 years ago.
     </p>
     <br />
     <h3>Event Details</h3>
@@ -44,7 +71,7 @@
       <h4>Address:</h4>
       <p>819 Abbot Rd, East Lansing, MI 48823</p>
       <h4>Cost:</h4>
-      <p>Free and Open to the Public</p>
+      <p>Seating Limited - Free and Open to the Public</p>
     </div>
     <p>
       Please bring your friends and family to celebrate this special occasion
